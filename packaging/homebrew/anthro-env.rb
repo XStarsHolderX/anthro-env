f86b6922d847f5ec85af7819a0b6569fe2ab4005
kelaocai <kelaocai@163.com>
2026-03-07 14:51:06 +0800

    chore: finalize homebrew formula metadata for v0.1.0-alpha

--- packaging/homebrew/anthro-env.rb
+++ packaging/homebrew/anthro-env.rb
@@ -1,8 +1,8 @@
 class AnthroEnv < Formula
   desc "macOS Anthropic environment profile manager"
-  homepage "https://github.com/anthro-env/anthro-env"
-  url "https://github.com/anthro-env/anthro-env/archive/refs/tags/v0.1.0-alpha.tar.gz"
-  sha256 "REPLACE_WITH_REAL_SHA256"
+  homepage "https://github.com/kelaocai/anthro-env"
+  url "https://github.com/kelaocai/anthro-env/archive/refs/tags/v0.1.0-alpha.tar.gz"
+  sha256 "336026c1009b5b18a830e17aa15f4806fb9f7f4e04a852e400cbda0ef6337ed3"
   license "MIT"
 
   depends_on "go" => :build
